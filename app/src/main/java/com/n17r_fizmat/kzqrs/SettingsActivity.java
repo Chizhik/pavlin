2f1bc31a213f7a1367c1feeb8a3e1f7a349f04bf
Alisher Tortay <t.b.alisher@gmail.com>
2016-07-20 18:00:36 +0600

    circular avatar

--- app/src/main/java/com/n17r_fizmat/kzqrs/SettingsActivity.java
+++ app/src/main/java/com/n17r_fizmat/kzqrs/SettingsActivity.java
@@ -3,6 +3,11 @@ package com.n17r_fizmat.kzqrs;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -116,6 +121,28 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -130,7 +157,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                     } catch(IOException e) {
                         e.printStackTrace();
                     }
-
+                    bm = getCroppedBitmap(bm);
                     profileImage.setImageBitmap(bm);
                     imageChanged = true;
                 }
