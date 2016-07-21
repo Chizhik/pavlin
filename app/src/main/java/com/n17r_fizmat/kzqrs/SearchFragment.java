b7058ba332e9434201e144b582fb97f1a3be2641
Alisher Tortay <t.b.alisher@gmail.com>
2016-07-21 17:02:04 +0600

    implemented OnItemClickListener on search listView

--- app/src/main/java/com/n17r_fizmat/kzqrs/SearchFragment.java
+++ app/src/main/java/com/n17r_fizmat/kzqrs/SearchFragment.java
@@ -2,21 +2,28 @@ package com.n17r_fizmat.kzqrs;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment implements SearchView.OnQueryTextListener {
+public class SearchFragment extends Fragment implements SearchView.OnQueryTextListener, AdapterView.OnItemClickListener {
 
     private ListView listView;
     SearchView searchView;
@@ -36,13 +43,14 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         searchView.setOnQueryTextListener(this);
 
         listView = (ListView) v.findViewById(R.id.searchListView);
+        listView.setOnItemClickListener(this);
         return v;
     }
 
     @Override
     public boolean onQueryTextSubmit(String query) {
         ProgressDialog pd = new ProgressDialog(getContext());
-        pd.setTitle("Мдет поиск");
+        pd.setTitle("Идет поиск");
         pd.setMessage("Пожалуйста подождите");
         pd.show();
         SearchParseAdapter mainAdapter = new SearchParseAdapter(getContext(), query);
@@ -56,4 +64,16 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         return false;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        ParseObject object = (ParseObject) listView.getItemAtPosition(i);
+        ParseUser user = (ParseUser) object;
+        Intent profileIntent = new Intent(getContext(), ProfileActivity.class);
+        Bundle b = new Bundle();
+        String id = user.getObjectId();
+        Log.d("ParseUser", "senderUser: " + id);
+        b.putString("ParseUserId", id);
+        profileIntent.putExtras(b);
+        startActivity(profileIntent);
+    }
 }
--- app/src/main/java/com/n17r_fizmat/kzqrs/SearchParseAdapter.java
+++ app/src/main/java/com/n17r_fizmat/kzqrs/SearchParseAdapter.java
@@ -26,7 +26,7 @@ import com.parse.ParseUser;
 /**
  * Created by Alisher on 7/15/2016.
  */
-public class SearchParseAdapter extends ParseQueryAdapter implements View.OnClickListener {
+public class SearchParseAdapter extends ParseQueryAdapter {
     ParseUser user;
     public SearchParseAdapter(Context context, final String searchQuery) {
         super(context, new ParseQueryAdapter.QueryFactory<ParseObject>() {
@@ -47,7 +47,6 @@ public class SearchParseAdapter extends ParseQueryAdapter implements View.OnClic
         super.getItemView(object, v, parent);
 
         final ImageView profileImage = (ImageView) v.findViewById(R.id.resultImage);
-        CardView card = (CardView) v.findViewById(R.id.resultCard);
         TextView username = (TextView) v.findViewById(R.id.resultText);
         try {
             user = (ParseUser) object;
@@ -72,36 +71,24 @@ public class SearchParseAdapter extends ParseQueryAdapter implements View.OnClic
             Log.v("Parse", e.toString());
             e.printStackTrace();
         }
-        card.setOnClickListener(this);
+//        card.setOnClickListener(this);
 
         return v;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.resultCard:
-                // TODO new fragment
-                Context c = getContext();
-                Intent profileIntent = new Intent(c, ProfileActivity.class);
-                Bundle b = new Bundle();
-                String id = user.getObjectId();
-                b.putString("ParseUserId", id);
-                profileIntent.putExtras(b);
-                c.startActivity(profileIntent);
-//                // Create new fragment and transaction
-//                Fragment newFragment = HomeFragment.newInstance(user);
-//                // consider using Java coding conventions (upper first char class names!!!)
-//                FragmentTransaction transaction = ((MainActivity)getContext()).getSupportFragmentManager().beginTransaction();
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.resultCard:
+//                Context c = getContext();
+//                Intent profileIntent = new Intent(c, ProfileActivity.class);
+//                Bundle b = new Bundle();
+//                String id = user.getObjectId();
+//                b.putString("ParseUserId", id);
+//                profileIntent.putExtras(b);
+//                c.startActivity(profileIntent);
 //
-//                // Replace whatever is in the fragment_container view with this fragment,
-//                // and add the transaction to the back stack
-//                transaction.replace(android.R.id.content, newFragment);
-//                transaction.addToBackStack(null);
-//
-//                // Commit the transaction
-//                transaction.commit();
-                break;
-        }
-    }
+//                break;
+//        }
+//    }
 }
