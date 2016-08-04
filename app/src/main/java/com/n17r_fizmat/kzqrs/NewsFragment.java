78284e828e1045f01f3bbcdab72c66153c82b5cd
Alisher Tortay <t.b.alisher@gmail.com>
2016-08-04 22:09:44 +0600

    news swiperefresh

--- app/src/main/java/com/n17r_fizmat/kzqrs/NewsFragment.java
+++ app/src/main/java/com/n17r_fizmat/kzqrs/NewsFragment.java
@@ -3,6 +3,7 @@ package com.n17r_fizmat.kzqrs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,10 @@ import android.widget.ListView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NewsFragment extends Fragment {
+public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     ListView news_list;
     NewsParseAdapter mainAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
 
     public NewsFragment() {
@@ -28,9 +30,17 @@ public class NewsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_news, container, false);
         news_list = (ListView) v.findViewById(R.id.news_list);
+        swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.news_swipe_refresh_layout);
         mainAdapter = new NewsParseAdapter(getContext());
         news_list.setAdapter(mainAdapter);
+        swipeRefreshLayout.setOnRefreshListener(this);
         return v;
     }
 
+    @Override
+    public void onRefresh() {
+        mainAdapter = new NewsParseAdapter(getContext());
+        news_list.setAdapter(mainAdapter);
+        swipeRefreshLayout.setRefreshing(false);
+    }
 }
