a2ff9b45a5fd97ada1c49cd36f8851b1203e677e
Alisher Tortay <t.b.alisher@gmail.com>
2016-08-05 11:08:50 +0600

    added scrollview to activity_share

--- app/src/main/java/com/n17r_fizmat/kzqrs/MainActivity.java
+++ app/src/main/java/com/n17r_fizmat/kzqrs/MainActivity.java
@@ -22,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragments(new NewsFragment(), "News");
         viewPagerAdapter.addFragments(new HomeFragment(), "Home");
+        viewPagerAdapter.addFragments(new NewsFragment(), "News");
         viewPagerAdapter.addFragments(new SearchFragment(), "Search");
         viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setCurrentItem(1);
         tabLayout.setupWithViewPager(viewPager);
     }
 
